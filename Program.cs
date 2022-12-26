122b632a5c1a9a0c526cf2b74669f581e4af9a46
Irina <bezdnaya@mail.ru>
2022-12-26 18:11:59 +0200

    Создали пустой массив и добавили туда элементы длиной до 4 символов

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Cоздать новый массив, который состоит из эдементов меншье или равно 3 символов из имеющегося массива.
+
+void FillArray (string[] collection)
+{
+    string[] Baza = new string [] {"a", "b", "c", "d", "e", "f", "g", "h", "q", "w", "r", "t", "y", "u", "i", "o", "p", "s", "j", "k", "l", "z", "x", "v", "n", "m"};
+    for (int i = 0; i < collection.Length; i++)
+    {
+        int elementLength = new Random().Next(1, 10);
+        string element = string.Empty;
+        for (int j = 0; j < elementLength; j++)
+        {
+            element += Baza[new Random().Next(0, Baza.Length)];
+        }
+        collection [i] = element;
+
+    }
+}
+void PrintArray (string [] coll)
+{
+    Console.WriteLine(string.Join(" ", coll));
+}
+string [] StartArray = new string [new Random().Next(1, 11)];
+FillArray(StartArray);
+PrintArray(StartArray);
+int len = 0;
+string [] ResArray = new string [len];
+for (int i = 0; i < StartArray.Length; i++)
+{
+    if (StartArray[i].Length < 4)
+    {
+        Array.Resize(ref ResArray, len + 1);
+        ResArray[len] = StartArray [i];
+        len ++;
+    }
+}
+PrintArray(ResArray);
+
+
+
